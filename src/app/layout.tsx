c6f42ceeb3adfff033f394adf8a85a2770e196f4
gimHyoungSoo <gimhyoungsoo@gmail.com>
2023-10-14 15:52:06 +0900

    ♻️ 디렉토리 구조 리팩토링

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 // import "./globals.css"
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Nav from "./common/Nav";
+import Nav from "../components/Nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
