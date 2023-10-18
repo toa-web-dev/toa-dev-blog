914a1ded47c68a971ca0966f7e1f75bdae3ed01e
gimHyoungSoo <gimhyoungsoo@gmail.com>
2023-10-18 18:08:08 +0900

    💡 캐싱 위치 주석 추가

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -10,6 +10,7 @@ export default async function Page({ params }: { params: PageParams }) {
   const splitArray = pathname.split('-');
   const currentPostTitle = splitArray.join(' ');
 
+  // 변수 html을 캐싱해 최적화 하기
   const postInfo = getPostBySlug(currentPostTitle, ['slug', 'content']);
   const html = await markdownToHtml(postInfo.content);
   
