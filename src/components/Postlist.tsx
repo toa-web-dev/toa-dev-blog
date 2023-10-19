33f2e910748712cc5628e3ce112f7b4f43542aef
gimHyoungSoo <gimhyoungsoo@gmail.com>
2023-10-19 21:41:40 +0900

    🐛 스프레드 문법으로 복사한 배열에 sort() 메서드 적용

--- src/components/Postlist.tsx
+++ src/components/Postlist.tsx
@@ -9,20 +9,24 @@ type PostlistProps = {
 };
 
 export default function Postlist({ data }: PostlistProps) {
-  const [orderedData, setOrderedData] = useState(data);
+  const newestData = [...data].sort((post1, post2) =>
+    post1.date > post2.date ? -1 : 1
+  );
+  const [orderedData, setOrderedData] = useState(newestData);
   const [order, setOrder] = useState('new');
 
   useEffect(() => {
     if (order === 'new') {
-      setOrderedData(
-        data.sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
+      setOrderedData((prev) =>
+        [...prev].sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
       );
     }
     if (order === 'old') {
-      setOrderedData(
-        data.sort((post1, post2) => (post1.date > post2.date ? 1 : -1))
+      setOrderedData((prev) =>
+        [...prev].sort((post1, post2) => (post1.date > post2.date ? 1 : -1))
       );
     }
+  
   }, [order]);
 
   return (
@@ -30,13 +34,21 @@ export default function Postlist({ data }: PostlistProps) {
       <button onClick={() => setOrder('new')}>new</button>
       <button onClick={() => setOrder('old')}>old</button>
       <ol>
-        {orderedData.map((el, idx) => (
-          typeof el.slug === 'string' &&  el.date instanceof Date &&
-          <li key={idx}>
-            <Link href={`/post/${el.slug.replace(/\s/g, '-')}`}>{el.slug}</Link>
-            <p>{el.date && `${el.date.getFullYear()}-${el.date.getMonth()}-${el.date.getDate()}`}</p>
-          </li>
-        ))}
+        {orderedData.map(
+          (el, idx) =>
+            typeof el.slug === 'string' &&
+            el.date instanceof Date && (
+              <li key={idx}>
+                <Link href={`/post/${el.slug.replace(/\s/g, '-')}`}>
+                  {el.slug}
+                </Link>
+                <p>
+                  {el.date &&
+                    `${el.date.getFullYear()}-${el.date.getMonth()}-${el.date.getDate()}`}
+                </p>
+              </li>
+            )
+        )}
       </ol>
     </>
   );
