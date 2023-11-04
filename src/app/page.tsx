3ffc299ae5eb15b7a3120e1e9a52e47de44ed8c3
gimHyoungSoo <gimhyoungsoo@gmail.com>
2023-11-04 16:40:09 +0900

    💄 Postlist 스타일 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default function Home() {
-    return <main>홈페이지</main>;
+  return (
+    <>
+      <main>홈페이지</main>
+    </>
+  );
 }
--- src/components/Postlist.tsx
+++ src/components/Postlist.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import { OrderType } from '../interface/post';
 import useOrderPosts from '../hook/useOrderPosts';
+import { ListGroup, ButtonGroup, ToggleButton } from 'react-bootstrap';
 
 type Props = {
   data: OrderType[];
@@ -15,26 +16,32 @@ export default function Postlist({ data }: Props) {
   const [orderData, setOrderData] = useState<OrderType[]>(newestData);
   const [order, setOrder] = useState('new');
 
-  useEffect(() => {
+  useEffect(() => {    
     setOrderData((prev) => useOrderPosts(prev, order));
   }, [order]);
 
   return (
     <>
-      <button onClick={() => setOrder('new')}>new</button>
-      <button onClick={() => setOrder('old')}>old</button>
-      <ol>
+      <ButtonGroup>
+        <ToggleButton id="1" type="radio" value="new" checked={order === 'new'} onChange={(e) => setOrder(e.currentTarget.value)}>
+          new
+        </ToggleButton>
+        <ToggleButton id="2" type="radio" value="old" checked={order === 'old'} onChange={(e) => setOrder(e.currentTarget.value)}>
+          old
+        </ToggleButton>
+      </ButtonGroup>
+      <ListGroup as="ol" variant="flush">
         {orderData.map((el, idx) => {
           const url = `/post/${el.slug.replace(/\s/g, '-')}`;
           const date = `${el.date.getFullYear()}-${el.date.getMonth()}-${el.date.getDate()}`;
           return (
-            <li key={idx}>
+            <ListGroup.Item as="li" action key={idx}>
               <Link href={url}>{el.slug}</Link>
               <p>{el.date && date}</p>
-            </li>
+            </ListGroup.Item>
           );
         })}
-      </ol>
+      </ListGroup>
     </>
   );
 }
