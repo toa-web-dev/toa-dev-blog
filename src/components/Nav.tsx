34c70fe487f401d92070c69105c4367af28fd8c5
gimHyoungSoo <gimhyoungsoo@gmail.com>
2023-11-10 17:48:17 +0900

    🚧 헤더 Nav 링크 수정

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Nav() {
     return (
         <nav>
-            <Link href="/">Home</Link>
-            <Link href="/post">Post</Link>
-            <Link href="/category">Category</Link>
+            <Link href="/">Home</Link> 
+            {/* <Link href="/about">About</Link> */}
+            <Link href="/blog">Blog</Link>
         </nav>
     );
 }
